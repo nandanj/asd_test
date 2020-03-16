fb8686fc47e0cffe4724f7e5533b186f9aff27c6
Bernard Tison <bernard.tison@gmail.com>
2020-03-16 12:30:34 +0100

    Spring Boot Kafka Producer: initial commit

---
+++ spring-boot-kafka-producer/src/main/java/org/acme/sb/kafka/KafkaProducerApplication.java
@@ -0,0 +1,15 @@
+package org.acme.sb.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class);
+    }
+
+}
